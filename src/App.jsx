17d976032736a5a5b3f9746668c0f987486f8919
Stanley J Delva <stanleyjdelva@Stanleys-MacBook-Pro.local>
2025-06-12 05:03:31 -0400

    implented the fliping of cards when when card is click

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,45 @@ import flashcards from "./FlashCardData";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-    const [cardData, setCardData] = useState(flashcards);
-    const [flip, setFlip] = useState(true);
+  const [count, setCount] = useState(0);
+  const [cardData, setCardData] = useState(flashcards);
 
-    function handleCardClick() {
-        if (flip) setFlip(false);
-
-        if (!flip) setFlip(true);
-    }
-
-    return (
-        <>
-            <div className="app-container">
-                <Card
-                    cardData={cardData}
-                    flip={flip}
-                    handleCardClick={handleCardClick}
-                />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="app-container">
+        <Card cardData={cardData} />
+      </div>
+    </>
+  );
 }
 
-function Card({ cardData, flip, handleCardClick }) {
-    return (
-        <div className={`card flip-card `} onClick={handleCardClick}>
-            {flip ? (
-                <div className="card-front">{cardData[0].name}</div>
-            ) : (
-                <div className="card-back">{cardData[0].facts[0]}</div>
-            )}
-        </div>
-    );
+function Card({ cardData }) {
+  const [flip, setFlip] = useState(true);
+
+  function handleCardClick() {
+    if (flip) setFlip(false);
+
+    if (!flip) setFlip(true);
+  }
+
+  return (
+    <div className={`flip-card`} onClick={handleCardClick}>
+      <div className={`flip-card-inner ${flip ? "" : "rotate"}`}>
+        <div className="flip-card-front">{cardData[0].name}</div>
+        <div className="flip-card-back">{cardData[0].facts[0]}</div>
+      </div>
+    </div>
+    // <div
+    //   className={`card ${flip ? "" : "flip-card"} `}
+    //   onClick={handleCardClick}
+    // >
+    //   {flip ? (
+    //     <div className="card-front">{cardData[0].name}</div>
+    //   ) : (
+    //     <div className="card-back">{cardData[0].facts[0]}</div>
+    //   )}
+    // </div>
+  );
 }
 
 export default App;
