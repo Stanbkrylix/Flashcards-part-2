277d08e4911d4ae0dc6d896547d9c799bc92c399
Stanley Delva <juniordelva1@gmail.com>
2025-06-12 14:36:54 -0400

    trying to implement the previous button functionality without any unintended effects

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,116 @@ import flashcards from "./FlashCardData";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [cardData, setCardData] = useState(flashcards);
+    const [count, setCount] = useState(0);
+    const [cardData, setCardData] = useState(flashcards);
+    const [currentCard, setCurrentCard] = useState(0);
 
-  return (
-    <>
-      <div className="app-container">
-        <Card cardData={cardData} />
-      </div>
-    </>
-  );
+    function onClickNextBtn() {
+        if (currentCard >= cardData.length - 1) return;
+        setCurrentCard(currentCard + 1);
+    }
+
+    function onClickPrevBtn() {
+        if (currentCard <= 0) return;
+        console.log(currentCard);
+        setCurrentCard(currentCard - 1);
+    }
+
+    function handleCardClick(currentCard) {
+        if (cardData[currentCard].flip) {
+            setCardData((prev) =>
+                prev.map((card, index) =>
+                    currentCard === index ? { ...card, flip: false } : card
+                )
+            );
+        }
+
+        if (!cardData[currentCard].flip) {
+            setCardData((prev) =>
+                prev.map((card, index) =>
+                    currentCard === index ? { ...card, flip: true } : card
+                )
+            );
+        }
+    }
+
+    function resetFlipStatus(currentCard) {
+        console.log(cardData[currentCard - 1]);
+        console.log(cardData[currentCard]);
+
+        console.log(cardData[currentCard - 1].flip === false);
+
+        // if (cardData[currentCard - 1].flip === false) {
+        //     setCardData((prev) =>
+        //         prev.map((card, index) =>
+        //             currentCard - 1 === index - 1
+        //                 ? { ...card, flip: true }
+        //                 : card
+        //         )
+        //     );
+        // }
+    }
+
+    return (
+        <>
+            <div className="app-container">
+                <Card
+                    cardData={cardData}
+                    currentCard={currentCard}
+                    handleCardClick={handleCardClick}
+                />
+                <NavigationBtns
+                    onClickPrevBtn={onClickPrevBtn}
+                    onClickNextBtn={onClickNextBtn}
+                    resetFlipStatus={resetFlipStatus}
+                    currentCard={currentCard}
+                />
+            </div>
+        </>
+    );
 }
 
-function Card({ cardData }) {
-  const [flip, setFlip] = useState(true);
+function Card({ cardData, currentCard, handleCardClick }) {
+    return (
+        <div className="flip-card" onClick={() => handleCardClick(currentCard)}>
+            <div
+                className={`flip-card-inner ${
+                    cardData[currentCard].flip ? "" : "rotate"
+                }`}
+            >
+                <div className="flip-card-front">
+                    {cardData[currentCard].name}
+                </div>
+                <div className="flip-card-back">
+                    {cardData[currentCard].facts[0]}
+                </div>
+            </div>
+        </div>
+    );
+}
 
-  function handleCardClick() {
-    if (flip) setFlip(false);
-
-    if (!flip) setFlip(true);
-  }
-
-  return (
-    <div className={`flip-card`} onClick={handleCardClick}>
-      <div className={`flip-card-inner ${flip ? "" : "rotate"}`}>
-        <div className="flip-card-front">{cardData[0].name}</div>
-        <div className="flip-card-back">{cardData[0].facts[0]}</div>
-      </div>
-    </div>
-    // <div
-    //   className={`card ${flip ? "" : "flip-card"} `}
-    //   onClick={handleCardClick}
-    // >
-    //   {flip ? (
-    //     <div className="card-front">{cardData[0].name}</div>
-    //   ) : (
-    //     <div className="card-back">{cardData[0].facts[0]}</div>
-    //   )}
-    // </div>
-  );
+function NavigationBtns({
+    onClickPrevBtn,
+    onClickNextBtn,
+    resetFlipStatus,
+    currentCard,
+}) {
+    return (
+        <div className="navigation-btn">
+            <button
+                className="previous-btn"
+                onClick={() => {
+                    onClickPrevBtn();
+                    resetFlipStatus(currentCard);
+                }}
+            >
+                Previous
+            </button>
+            <button className="next-btn" onClick={onClickNextBtn}>
+                Next
+            </button>
+        </div>
+    );
 }
 
 export default App;
